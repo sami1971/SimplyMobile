8a44fec3540b0117d5a2c1fec4ed3459d2463b5b
Sami M. Kallio <code.kallio@gmail.com>
2013-09-29 09:30:19 -0400

    ObservableDataSource changes

--- Android/SimplyMobile.Data/ObservableDataSource.cs
+++ Android/SimplyMobile.Data/ObservableDataSource.cs
@@ -13,55 +13,53 @@ using Android.Widget;
 
 namespace SimplyMobile.Data
 {
+    /// <summary>
+    /// Observable data source.
+    /// </summary>
     public partial class ObservableDataSource : Java.Lang.Object, IListAdapter, ISpinnerAdapter
     {
-
         /// <summary>
-        /// The collection changed partial method must be implemented in the OS specific code.
+        /// The are all items enabled.
         /// </summary>
-        /// <param name="sender">
-        /// The sender.
-        /// </param>
-        /// <param name="notifyCollectionChangedEventArgs">
-        /// The notify collection changed event args.
-        /// </param>
-        partial void CollectionChanged(object sender, NotifyCollectionChangedEventArgs notifyCollectionChangedEventArgs)
-        {
-            foreach (var listView in this.observers.OfType<ListView>())
-            {
-                
-            }
-        }
-
-        /// <summary>
-        /// Partial class to be implemented in the OS specific code.
-        /// </summary>
-        /// <param name="sender">
-        /// The sender.
-        /// </param>
-        /// <param name="notifyCollectionChangedEventArgs">
-        /// The notify collection changed event args.
-        /// </param>
-        partial void ObserversChanged(object sender, NotifyCollectionChangedEventArgs notifyCollectionChangedEventArgs)
-        {
-            
-        }
-
+        /// <returns>
+        /// <see cref="bool"/>.
+        /// </returns>
         public bool AreAllItemsEnabled()
         {
             return true;
         }
 
+        /// <summary>
+        /// Return whether item is enabled.
+        /// </summary>
+        /// <param name="position">
+        /// The position.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
         public bool IsEnabled(int position)
         {
             return true;
         }
 
+        /// <summary>
+        /// Gets the count.
+        /// </summary>
         public int Count
         {
             get { return this.Data.Count; }
         }
 
+        /// <summary>
+        /// Gets the item.
+        /// </summary>
+        /// <param name="position">
+        /// The position.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Object"/>.
+        /// </returns>
         public Java.Lang.Object GetItem(int position)
         {
             return position;
@@ -112,6 +110,37 @@ namespace SimplyMobile.Data
         public virtual View GetDropDownView(int position, View convertView, ViewGroup parent)
         {
             return this.GetView(position, convertView, parent);
+        }
+
+        /// <summary>
+        /// The collection changed partial method Android implementation.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="notifyCollectionChangedEventArgs">
+        /// The notify collection changed event args.
+        /// </param>
+        partial void CollectionChanged(object sender, NotifyCollectionChangedEventArgs notifyCollectionChangedEventArgs)
+        {
+            foreach (var listView in this.observers.OfType<ListView>())
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Observers changed partial method Android implementation.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="notifyCollectionChangedEventArgs">
+        /// Changed observer information.
+        /// </param>
+        partial void ObserversChanged(object sender, NotifyCollectionChangedEventArgs notifyCollectionChangedEventArgs)
+        {
+
         }
     }
 }
