d5778f9a84295faaeb0f554ea6d742ec8b1b3ff8
Sami M. Kallio <code.kallio@gmail.com>
2014-01-08 01:23:22 -0500

    Xml serializer added to Android

---
+++ Android/Tests/TextSerializationTests/ServiceStackXmlTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimplyMobile.Text.ServiceStack;
+
+namespace TextSerializationTests
+{
+    public class ServiceStackXmlTests : TestBase
+    {
+        protected override SimplyMobile.Text.ITextSerializer Serializer
+        { 
+            get { return new XmlSerializer(); }
+        }
+
+        protected override SimplyMobile.Text.ITextSerializer Deserializer
+        {
+            get { return new XmlSerializer(); }
+        }
+    }
+}
