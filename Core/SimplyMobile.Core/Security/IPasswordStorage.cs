29617f7327fce4738d7294023ebee935352a3c32
Sami M. Kallio <code.kallio@gmail.com>
2014-02-19 17:33:04 -0500

    Password storage interface

---
+++ Core/SimplyMobile.Core/Security/IPasswordStorage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimplyMobile.Core
+{
+	public interface IPasswordStorage
+	{
+		bool DeletePassword(string username, string service);
+		bool SetPassword(string username, string service, string password);
+		bool TryGetPassword(string username, string service, out string password);
+	}
+}
+
