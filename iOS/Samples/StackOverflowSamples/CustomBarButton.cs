fb7564f9516cd0be069dc32b4eacb7be6e627873
Sami M. Kallio <code.kallio@gmail.com>
2014-03-04 09:44:02 -0500

    Misc updates

---
+++ iOS/Samples/StackOverflowSamples/CustomBarButton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MonoTouch.UIKit;
+using MonoTouch.CoreGraphics;
+using System.Drawing;
+
+using MonoTouch.CoreImage;
+
+namespace StackOverflowSamples
+{
+	public class CustomBarButton : UIBarButtonItem
+	{
+		public new event EventHandler Clicked
+		{
+			add
+			{
+//				base.Clicked += value;
+			}
+			remove
+			{
+//				base.Clicked -= value;
+			}
+		}
+
+		public static RectangleF GetInfinite()
+		{
+			var image = CIImage.EmptyImage;
+
+			if (image.Extent.IsInfinite ())
+			{
+				return image.Extent;
+			}
+
+			throw new Exception ("Unable to create infinite rect");
+		}
+	}
+}
+
