b9f3dd01aaec55101d3bd4775776627e1a8b2387
Sami M. Kallio <code.kallio@gmail.com>
2014-03-05 17:37:39 -0500

    Twitter sample updated

---
+++ iOS/Samples/TwitterSample/TwitterTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using MonoTouch.UIKit;
+using SimplyMobile.Data;
+using MonoTouch.Foundation;
+
+namespace TwitterSample
+{
+	[Register("TwitterTable")]
+	public class TwitterTable : UITableView, ITableCellProvider<Datum>
+	{
+		public TwitterTable ()
+		{
+		}
+
+		public TwitterTable (IntPtr handle) : base(handle)
+		{
+		}
+
+		#region ITableCellProvider implementation
+
+		public UITableViewCell GetCell (Datum item)
+		{
+			var newCell = this.DequeueReusableCell(TwitterTableCell.Key) 
+				as TwitterTableCell ?? TwitterTableCell.Create();
+
+			newCell.Bind (item);
+
+			return newCell;
+		}
+
+		public float GetHeightForRow (NSIndexPath indexPath)
+		{
+			return 114f;
+		}
+
+		#endregion
+	}
+}
+
---
+++ iOS/Samples/TwitterSample/TwitterTableCell.designer.cs
@@ -0,0 +1,58 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
+
+namespace TwitterSample
+{
+	[Register ("TwitterTableCell")]
+	partial class TwitterTableCell
+	{
+		[Outlet]
+		MonoTouch.UIKit.UIButton buttonViewComments { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIImageView image { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel labelCaption { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel labelType { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel labelUser { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (image != null) {
+				image.Dispose ();
+				image = null;
+			}
+
+			if (labelCaption != null) {
+				labelCaption.Dispose ();
+				labelCaption = null;
+			}
+
+			if (labelUser != null) {
+				labelUser.Dispose ();
+				labelUser = null;
+			}
+
+			if (labelType != null) {
+				labelType.Dispose ();
+				labelType = null;
+			}
+
+			if (buttonViewComments != null) {
+				buttonViewComments.Dispose ();
+				buttonViewComments = null;
+			}
+		}
+	}
+}
