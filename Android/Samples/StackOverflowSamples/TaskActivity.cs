66fda2238c35d147d8b5730c09bc3cf6d2578dce
Sami M. Kallio <code.kallio@gmail.com>
2014-03-13 23:06:31 -0400

    1

---
+++ Android/Samples/StackOverflowSamples/TaskActivity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Threading.Tasks;
+
+namespace StackOverflowSamples
+{
+    [Activity (Label = "TaskActivity")]			
+    public class TaskActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate (bundle);
+
+            // Create your application here
+        }
+
+        protected override async void OnResume()
+        {
+            base.OnResume ();
+
+            StartAnimate ();
+
+            await RunAsync (TimeSpan.FromSeconds (4));
+
+            StopAnimate ();
+        }
+
+        private async Task RunAsync(TimeSpan span)
+        {
+            await Task.Delay (span);
+        }
+
+        private void StartAnimate()
+        {
+            // put animation here
+        }
+
+        private void StopAnimate()
+        {
+            // stop animating after the thread has ended
+        }
+    }
+}
+
