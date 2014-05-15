e692c68bcba7fa793f93788867c9e7f5cf83e7ed
Sami M. Kallio <code.kallio@gmail.com>
2014-05-15 16:11:13 -0400

    iOS device type enums

---
+++ iOS/SimplyMobile.Device/DeviceType.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace SimplyMobile.Device
+{
+    public enum DeviceType
+    {
+        iPhone,
+        iPad,
+        iPod,
+        Simulator
+    }
+
+    public enum HardwareVersion 
+    {
+        iPhone1G,
+        iPhone3G,
+        iPhone3GS,
+        iPhone4_GSM,
+        iPhone4_CDMA,
+        iPhone4S,
+        iPhone5_GSM,
+        iPhone5_CDMA,
+        iPhone5C,
+        iPhone5S,
+        iPod1G,
+        iPod2G,
+        iPod3G,
+        iPod4G,
+        iPod5G,
+        iPad1G,
+        iPad2WIFI,
+        iPad2GSM,
+        iPad2CDMA,
+        iPadMiniWIFI,
+        iPadMiniGSM,
+        iPadMiniCDMA,
+        iPad3WIFI,
+        iPad3GSM,
+        iPad3CDMA,
+        iPad4WIFI,
+        iPad4GSM,
+        iPad4CDMA,
+        iPadAirWIFI,
+        iPadAirGSM,
+        iPadAirCDMA,
+        iPadMiniRetinaWifi,
+        iPadMiniRetinaCDMA,
+        iPhoneSimulator,
+        iPhone4Simulator,
+        iPadSimulator,
+        Unknown
+    }
+}
+
